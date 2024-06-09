cc97d93b02832e437ba53b960d272690fc5a96be
Михаел Йорданов <gaws@mail.bg>
2024-06-09 19:50:26 +0300

    Add  static Validation Constants

---
+++ SelfGuidedTours.Common/ValidationConstants/ValidationConstants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Common.ValidationConstants
+{
+    public static class ValidationConstants
+    {
+        public static class Tour
+        {
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 50;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 500;
+
+            public const decimal PriceMinValue = 1.0m;
+        }
+
+        public static class Landmark
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 50;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 500;
+
+            public const int HistoryMinLength = 10;
+            public const int HistoryMaxLength = 500;
+
+        }
+
+        public static class Review
+        {
+            public const int CommentMinLength = 10;
+            public const int CommentMaxLength = 500;
+        }
+    }
+}
