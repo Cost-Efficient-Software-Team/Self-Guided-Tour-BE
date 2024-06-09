4693a6343959ef9562e135aeb21046bef9b9d3ec
Михаел Йорданов <gaws@mail.bg>
2024-06-09 22:47:06 +0300

    Add a controller to test if db is working in container

---
+++ SelfGuidedTours.Api/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SelfGuidedTours.Infrastructure.Data;
+
+namespace SelfGuidedTours.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly SelfGuidedToursDbContext context;
+
+        public TestController(SelfGuidedToursDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public  IActionResult Get()
+        {
+            var tours = context.Tours;
+            return Ok(tours);
+        }
+
+
+
+    }
+}
