ef79f834221cb8ff34ade39bad782670620a766b
Михаел Йорданов <gaws@mail.bg>
2024-06-09 20:03:59 +0300

    Remove and sort usings

--- SelfGuidedTours.Common/ValidationConstants/ValidationConstants.cs
+++ SelfGuidedTours.Common/ValidationConstants/ValidationConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SelfGuidedTours.Common.ValidationConstants
+﻿namespace SelfGuidedTours.Common.ValidationConstants
 {
     public static class ValidationConstants
     {
