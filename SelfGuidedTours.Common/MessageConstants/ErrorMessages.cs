318a80e200b7186439a71ac1b4fd98655d29a896
Михаел Йорданов <gaws@mail.bg>
2024-06-09 19:50:45 +0300

    Add static error message constants

---
+++ SelfGuidedTours.Common/MessageConstants/ErrorMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Common.MessageConstants
+{
+    public static class ErrorMessages
+    {
+        public const string LengthErrorMessage = "{0} must be between {2} and {1} characters long!";
+
+        public const string CommentLengthErrorMessage = "{0} should not be more than {1} characters long!";
+    }
+}
