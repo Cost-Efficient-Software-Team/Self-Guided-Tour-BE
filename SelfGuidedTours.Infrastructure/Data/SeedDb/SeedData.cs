5fa0e63e94ba6dabc3935568be03ebd6a3a6d3cd
Михаел Йорданов <gaws@mail.bg>
2024-06-10 22:13:16 +0300

    Add user config to seed Admin user

---
+++ SelfGuidedTours.Infrastructure/Data/SeedDb/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SelfGuidedTours.Infrastructure.Data.Models;
+
+namespace SelfGuidedTours.Infrastructure.Data.SeedDb
+{
+    internal class SeedData
+    {
+        public ICollection<ApplicationUser> Users { get; set; } = new HashSet<ApplicationUser>();
+        public SeedData()
+        {
+           SeedAdminUser();
+        }
+        private void SeedAdminUser()
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            var adminUser = new ApplicationUser
+            {
+                Email = "admin@selfguidedtours.bg",
+                NormalizedEmail = "admin@selfguidedtours.bg".ToUpper(),
+                Name = "Admin Adminov",
+                NormalizedUserName = "Admin Adminov".ToUpper(),
+                PasswordHash = hasher.HashPassword(null!, "D01Parola")
+            };
+
+            Users.Add(adminUser);                
+        }
+    }
+}
