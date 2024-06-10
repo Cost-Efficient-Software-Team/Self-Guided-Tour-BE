1f909f7818e1dc19d91863ca428696ac89e316db
Михаел Йорданов <gaws@mail.bg>
2024-06-10 22:56:17 +0300

    remove comments

--- SelfGuidedTours.Infrastructure/Data/SelfGuidedToursDbContext.cs
+++ SelfGuidedTours.Infrastructure/Data/SelfGuidedToursDbContext.cs
@@ -6,7 +6,7 @@ using SelfGuidedTours.Infrastructure.Data.SeedDb;
 
 namespace SelfGuidedTours.Infrastructure.Data
 {
-    public class SelfGuidedToursDbContext:IdentityDbContext
+    public class SelfGuidedToursDbContext : IdentityDbContext
     {
         public SelfGuidedToursDbContext(DbContextOptions<SelfGuidedToursDbContext> options) : base(options)
         {
@@ -14,33 +14,17 @@ namespace SelfGuidedTours.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //TODO: move these in sepparate configuration classes
-            //modelBuilder.Entity<TourLandmark>()
-            //    .HasKey(tl => new { tl.TourId, tl.LandmarkId });
+            //Apply entity configurations
             modelBuilder.ApplyConfiguration(new TourLandmarkConfiguration());
-           // modelBuilder.Entity<Payment>()
-           //.HasOne(p => p.Tour)
-           //.WithMany(t => t.Payments)
-           //.HasForeignKey(p => p.TourId)
-           //.OnDelete(DeleteBehavior.Restrict);
-           modelBuilder.ApplyConfiguration(new PaymentConfiguration());
-            //modelBuilder.Entity<Review>()
-            //.HasOne(r => r.Tour)
-            //.WithMany(t => t.Reviews)
-            //.HasForeignKey(r => r.TourId)
-            //.OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.ApplyConfiguration(new PaymentConfiguration());
             modelBuilder.ApplyConfiguration(new ReviewConfiguration());
-            // modelBuilder.Entity<UserTours>()
-            //.HasOne(ut => ut.Tour)
-            //.WithMany(t => t.UserTours)
-            //.HasForeignKey(ut => ut.TourId)
-            //.OnDelete(DeleteBehavior.Restrict);
             modelBuilder.ApplyConfiguration(new UserToursConfiguration());
             modelBuilder.ApplyConfiguration(new RolesConfiguration());
             modelBuilder.ApplyConfiguration(new UsersConfiguration());
             modelBuilder.ApplyConfiguration(new UsersRolesConfiguration());
+
             base.OnModelCreating(modelBuilder);
-            
+
         }
 
         public DbSet<Coordinate> Coordinates { get; set; }
