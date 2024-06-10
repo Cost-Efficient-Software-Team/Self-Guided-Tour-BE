9e4ead33510bc1c9ea59d755afaf78d954aeabce
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-06-11 02:17:49 +0300

    Added Login functionality with JWT Authentication and test-login [HttpGet] method in AuthController.cs

--- SelfGuidedTours.Common/ValidationConstants/AuthConstants.cs
+++ SelfGuidedTours.Common/ValidationConstants/AuthConstants.cs
@@ -4,13 +4,12 @@
     {
         public const string RequiredMessage = "{0} field is required!";
         public const string LengthErrorMessage = "{0} must be at least {1} characters long!";
+        public const string InvalidEmailAddressMessage = "Invalid Email Address!";
         public static class Register
         {
             public const int PasswordMinLength = 6;
 
             public const string PasswordsDoNotMatchMessage = "Passwords do not match!";
-
-            public const string InvalidEmailAddressMessage = "Invalid Email Address!";
         }
     }
 }
---
+++ SelfGuidedTours.Core/Models/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SelfGuidedTours.Core.Models
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string ResponseMessage { get; set; } = null!;
+    }
+}
