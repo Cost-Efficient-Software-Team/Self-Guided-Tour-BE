3e256b0844aeed9772b085891e410bded332af37
Михаел Йорданов <gaws@mail.bg>
2024-06-12 11:53:45 +0300

    Implement EmailService IEmailInterface and SendEmaiDTO

---
+++ SelfGuidedTours.Core/Contracts/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using SelfGuidedTours.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Core.Contracts
+{
+    public interface IEmailService
+    {
+        Task SendEmail(SendEmailDto sendEmailRequest,string emailBodyFormat);
+    }
+}
