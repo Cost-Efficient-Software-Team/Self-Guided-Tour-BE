285ab386a677bb112f46dd14e6466f52a77b7711
Михаел Йорданов <gaws@mail.bg>
2024-06-17 11:10:37 +0300

    implement logic to validate google id token and create user if neccesery

---
+++ SelfGuidedTours.Core/Contracts/IGoogleAuthService.cs
@@ -0,0 +1,16 @@
+﻿using SelfGuidedTours.Core.Models.Auth;
+using SelfGuidedTours.Core.Models.ExternalLogin;
+using SelfGuidedTours.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Core.Contracts
+{
+    public interface IGoogleAuthService
+    {
+        Task<AuthenticateResponse> GoogleSignIn(GoogleSignInVM model);
+    }
+}
---
+++ SelfGuidedTours.Core/Services/GoogleAuthService.cs
@@ -0,0 +1,48 @@
+﻿using SelfGuidedTours.Core.Contracts;
+using SelfGuidedTours.Core.Models.Auth;
+using SelfGuidedTours.Core.Models.ExternalLogin;
+using System.Net.Http.Headers;
+using System.Text.Json;
+namespace SelfGuidedTours.Core.Services
+{
+    public class GoogleAuthService : IGoogleAuthService
+    {
+        private readonly IAuthService authService;
+
+        public GoogleAuthService(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+        public async Task<AuthenticateResponse> GoogleSignIn(GoogleSignInVM model)
+        {
+            var payload = await ValidateGoogleIdToken(model.IdToken);
+            if (payload == null)
+            {
+                throw new Exception("Invalid Google Id Token");
+            }
+            var googleUser = JsonSerializer.Deserialize<GoogleUserDto>(payload, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+           
+            var userResponse = await authService.GoogleSignInAsync(googleUser);
+
+            return userResponse;
+        }
+        private async Task<string?> ValidateGoogleIdToken(string idToken)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", idToken);
+
+            var response = await httpClient.GetAsync("https://www.googleapis.com/oauth2/v2/userinfo");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var userInfo = await response.Content.ReadAsStringAsync();
+
+            return userInfo;
+        }
+    }
+}
