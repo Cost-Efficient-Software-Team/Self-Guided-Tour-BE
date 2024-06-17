24f81ec68e8ed000e51db9ce5902bb0506df2c4a
Михаел Йорданов <gaws@mail.bg>
2024-06-17 11:08:47 +0300

    Setup DTOs

---
+++ SelfGuidedTours.Core/Models/ExternalLogin/GoogleSignInVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SelfGuidedTours.Core.Models.ExternalLogin
+{
+    public class GoogleSignInVM
+    {
+        [Required]
+        public string IdToken { get; set; } = null!;
+    }
+}
