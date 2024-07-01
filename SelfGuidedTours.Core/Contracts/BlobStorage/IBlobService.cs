6d6ce35d139643b9de0abc2fd76aaddaa59f3569
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-07-01 14:08:08 +0300

    Implemented BlobService for Azure BLOB Storage

---
+++ SelfGuidedTours.Core/Contracts/BlobStorage/IBlobService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SelfGuidedTours.Core.Contracts.BlobStorage
+{
+    public interface IBlobService
+    {
+        Task UploadFileAsync(IFormFile file, string blobName);
+        Task DeleteFileAsync(string blobName);
+        string GetFileUrl(string blobName);   
+    }
+}
