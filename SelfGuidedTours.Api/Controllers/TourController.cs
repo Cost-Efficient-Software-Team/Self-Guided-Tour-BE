9e6caca63764baf4cb908843d971e8f9e5b98a5e
Georgi Markov <gogata1905@gmail.com>
2024-07-03 03:50:24 +0300

    add GetTourById

--- SelfGuidedTours.Api/Controllers/TourController.cs
+++ SelfGuidedTours.Api/Controllers/TourController.cs
@@ -1,9 +1,9 @@
 ﻿    using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SelfGuidedTours.Core.Contracts;
 using SelfGuidedTours.Core.Models.Dto;
 using SelfGuidedTours.Core.Models;
-using SelfGuidedTours.Core.Services;
 using SelfGuidedTours.Infrastructure.Data.Models;
 using System.Net;
 using System.Security.Claims;
@@ -37,84 +37,32 @@ namespace SelfGuidedTours.Api.Controllers
             var creatorId = User.Claims.First().Value;
 
             var result = await _tourService.AddAsync(tourCreateDTO, creatorId);
-          
+
             return CreatedAtAction(nameof(CreateTour), new { id = ((Tour)result.Result).TourId }, result);
         }
 
-        //[HttpGet("{id:int}", Name = "GetTour")]
-        //public async Task<IActionResult> GetTour(int id)
-        //{
-        //    if (id == 0)
-        //    {
-        //        _response.StatusCode = HttpStatusCode.BadRequest;
-        //        _response.IsSuccess = false;
-        //        return BadRequest(_response);
-        //    }
+        [HttpGet("{id:int}", Name = "GetTour")]
+        public async Task<IActionResult> GetTour(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
 
-        //    var tour = await _tourService.GetTourById(id);
-        //    if (tour == null)
-        //    {
-        //        _response.StatusCode = HttpStatusCode.NotFound;
-        //        _response.IsSuccess = false;
-        //        return NotFound(_response);
-        //    }
+            var tour = await _tourService.GetTourById(id);
+            if (tour == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.IsSuccess = false;
+                return NotFound(_response);
+            }
 
-        //    _response.Result = tour;
-        //    _response.StatusCode = HttpStatusCode.OK;
-        //    return Ok(_response);
-        //}
+            _response.Result = tour;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
 
-        //[HttpGet]
-        //public async Task<IActionResult> GetTours()
-        //{
-        //    var tours = await _tourService.GetAllTours();
-        //    _response.Result = tours;
-        //    _response.StatusCode = HttpStatusCode.OK;
-        //    return Ok(_response);
-        //}
-
-        //[HttpPut("{id}")]
-        //public async Task<ActionResult<ApiResponse>> UpdateTour(int id, [FromForm] TourUpdateDTO tourUpdateDTO)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var result = await _tourService.UpdateTour(id, tourUpdateDTO);
-        //        return Ok(result);
-        //    }
-        //    else
-        //    {
-        //        _response.IsSuccess = false;
-        //        _response.StatusCode = HttpStatusCode.BadRequest;
-        //        _response.ErrorMessages = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
-        //        return BadRequest(_response);
-        //    }
-        //}
-
-        //[HttpDelete("{id:int}")]
-        //public async Task<IActionResult> DeleteTour(int id)
-        //{
-        //    if (id == 0)
-        //    {
-        //        _response.StatusCode = HttpStatusCode.BadRequest;
-        //        _response.IsSuccess = false;
-        //        return BadRequest(_response);
-        //    }
-
-        //    var result = await _tourService.DeleteTour(id);
-
-        //    if (!result.IsSuccess)
-        //    {
-        //        if (result.StatusCode == HttpStatusCode.BadRequest)
-        //        {
-        //            return BadRequest(result);
-        //        }
-        //        else if (result.StatusCode == HttpStatusCode.NotFound)
-        //        {
-        //            return NotFound(result);
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
     }
 }
--- SelfGuidedTours.Core/Services/TourService.cs
+++ SelfGuidedTours.Core/Services/TourService.cs
@@ -1,14 +1,12 @@
-﻿using SelfGuidedTours.Core.Models.Dto;
-using SelfGuidedTours.Core.Models;
-using SelfGuidedTours.Infrastructure.Data.Enums;
-using System.Net;
+﻿using SelfGuidedTours.Core.Contracts.BlobStorage;
 using SelfGuidedTours.Core.Contracts;
+using SelfGuidedTours.Core.Models.Dto;
+using SelfGuidedTours.Core.Models;
 using SelfGuidedTours.Infrastructure.Common;
 using SelfGuidedTours.Infrastructure.Data.Models;
-using SelfGuidedTours.Core.Contracts.BlobStorage;
+using System.Net;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;
-using Org.BouncyCastle.Asn1.Cmp;
+using SelfGuidedTours.Infrastructure.Data.Enums;
 
 namespace SelfGuidedTours.Core.Services
 {
@@ -20,9 +18,9 @@ namespace SelfGuidedTours.Core.Services
 
         public TourService(IRepository repository, IBlobService blobService)
         {
-             this.repository = repository;
-             this.blobService = blobService;
-             response = new ApiResponse();
+            this.repository = repository;
+            this.blobService = blobService;
+            response = new ApiResponse();
         }
 
         public async Task<ApiResponse> AddAsync(TourCreateDTO model, string creatorId)
@@ -77,7 +75,7 @@ namespace SelfGuidedTours.Core.Services
                 };
 
                 tourToAdd.Landmarks.Add(landmarkToAdd);
-                
+
                 await repository.AddAsync(landmarkToAdd);
                 await repository.SaveChangesAsync();
 
@@ -88,7 +86,7 @@ namespace SelfGuidedTours.Core.Services
                 var landmarkResources = landmark.Resources;
                 foreach (var resource in landmarkResources!)
                 {
-                    if(resource.Length > 0)
+                    if (resource.Length > 0)
                     {
                         var blobName = $"{Guid.NewGuid()}_{resource.FileName}";
                         await blobService.UploadFileAsync(resource, blobName);
@@ -111,6 +109,7 @@ namespace SelfGuidedTours.Core.Services
 
             return response;
         }
+
         private ResourceType GetResourceType(string contentType)
         {
             return contentType switch
@@ -125,76 +124,14 @@ namespace SelfGuidedTours.Core.Services
             };
         }
 
-        //public async Task<Tour> GetTourById(int id)
-        //{
-        //    return await _context.Tours
-        //        .Include(t => t.Landmarks)
-        //        .Include(t => t.Payments)
-        //        .Include(t => t.Reviews)
-        //        .Include(t => t.UserTours)
-        //        .FirstOrDefaultAsync(t => t.TourId == id);
-        //}
-
-        //public async Task<List<Tour>> GetAllTours()
-        //{
-        //    return await _context.Tours
-        //        .Include(t => t.Landmarks)
-        //        .Include(t => t.Payments)
-        //        .Include(t => t.Reviews)
-        //        .Include(t => t.UserTours)
-        //        .ToListAsync();
-        //}
-
-        //public async Task<ApiResponse> UpdateTour(int id, TourUpdateDTO tourUpdateDTO)
-        //{
-        //    var response = new ApiResponse();
-
-        //    var tour = await _context.Tours.FindAsync(id);
-        //    if (tour == null)
-        //    {
-        //        response.StatusCode = HttpStatusCode.NotFound;
-        //        response.IsSuccess = false;
-        //        return response;
-        //    }
-
-        //    tour.Title = tourUpdateDTO.Title;
-        //    tour.Description = tourUpdateDTO.Description;
-        //    tour.Price = tourUpdateDTO.Price;
-        //    tour.Location = tourUpdateDTO.Location;
-        //    tour.ThumbnailImageUrl = tourUpdateDTO.ThumbnailImageUrl;
-        //    tour.EstimatedDuration = tourUpdateDTO.EstimatedDuration;
-        //    tour.UpdatedAt = DateTime.Now;
-        //    tour.Status = Status.Pending;
-
-        //    _context.Entry(tour).State = EntityState.Modified;
-        //    await _context.SaveChangesAsync();
-
-        //    response.StatusCode = HttpStatusCode.NoContent;
-        //    return response;
-        //}
-
-        //public async Task<ApiResponse> DeleteTour(int id)
-        //{
-        //    var response = new ApiResponse();
-
-        //    var tour = await _context.Tours.FindAsync(id);
-        //    if (tour == null)
-        //    {
-        //        response.StatusCode = HttpStatusCode.BadRequest;
-        //        response.IsSuccess = false;
-        //        return response;
-        //    }
-
-        //    _context.Tours.Remove(tour);
-        //    await _context.SaveChangesAsync();
-
-        //    response.StatusCode = HttpStatusCode.NoContent;
-        //    return response;
-        //}
-
-        //public bool TourExists(int id)
-        //{
-        //    return _context.Tours.Any(e => e.TourId == id);
-        //}
+        public async Task<Tour> GetTourById(int id)
+        {
+            return await repository.AllReadOnly<Tour>()
+                .Include(t => t.Landmarks)
+                .Include(t => t.Payments)
+                .Include(t => t.Reviews)
+                .Include(t => t.UserTours)
+                .FirstOrDefaultAsync(t => t.TourId == id);
+        }
     }
 }
