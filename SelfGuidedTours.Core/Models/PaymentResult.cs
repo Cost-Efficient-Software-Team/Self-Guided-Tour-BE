4102ebb67bf50a31bebb984e678ffd8a3a51b432
Georgi Markov <gogata1905@gmail.com>
2024-07-13 12:55:35 +0300

    add models: PaymentResult and PaymentRequest

---
+++ SelfGuidedTours.Core/Models/PaymentResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SelfGuidedTours.Core.Models
+{
+    public class PaymentResult
+    {
+        public string PaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
