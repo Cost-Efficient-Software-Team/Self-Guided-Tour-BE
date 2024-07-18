b4b2fc867bf710f4a36bae58bcafb3c9f45f837b
Михаел Йорданов <gaws@mail.bg>
2024-07-18 23:11:39 +0300

    Add custom Exception for Email already exists

---
+++ SelfGuidedTours.Core/CustomExceptions/EmailAlreadyInUseException.cs
@@ -0,0 +1,11 @@
+﻿using static SelfGuidedTours.Common.MessageConstants.ErrorMessages;
+namespace SelfGuidedTours.Core.CustomExceptions
+{
+    public class EmailAlreadyInUseException : Exception
+    {
+        public EmailAlreadyInUseException() : base(UserWithEmailAlreadyExistsErrorMessage)
+        {
+
+        }
+    }
+}
