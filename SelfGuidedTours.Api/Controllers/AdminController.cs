d2270e29ff4954ca1a148b293ac4dfce33415ff4
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-07-29 23:49:29 +0300

    Set authorization only for admin in AdminController

--- SelfGuidedTours.Api/Controllers/AdminController.cs
+++ SelfGuidedTours.Api/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SelfGuidedTours.Core.Contracts;
 using SelfGuidedTours.Core.Models;
 using SelfGuidedTours.Infrastructure.Data.Enums;
@@ -6,6 +7,7 @@ using SelfGuidedTours.Infrastructure.Data.Enums;
 namespace SelfGuidedTours.Api.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
     [ApiController]
     public class AdminController : ControllerBase
     {
