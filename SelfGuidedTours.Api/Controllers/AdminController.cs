1bc1bc4d0219a08c296ad6646773bb10dba01681
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-07-29 23:46:06 +0300

    Implemented the logic in the new created AdminController for All Tours for Admin

---
+++ SelfGuidedTours.Api/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelfGuidedTours.Core.Contracts;
+using SelfGuidedTours.Core.Models;
+using SelfGuidedTours.Infrastructure.Data.Enums;
+
+namespace SelfGuidedTours.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService adminService;
+        public AdminController(IAdminService adminService)
+        {
+            this.adminService = adminService;
+        }
+
+        [HttpGet("all-tours")]
+        public async Task<IActionResult> AllTours(Status status)
+        {
+            var result = await adminService.GetAllToursAsync(status);
+
+            return Ok(result);
+        }
+    }
+}
