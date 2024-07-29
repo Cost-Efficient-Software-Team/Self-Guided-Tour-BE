818c5fccd0ac1acfe0a02fdca5d2aa1876a7344a
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-07-29 23:36:03 +0300

    Created DTO model for All Tours that must be returned to admin with particular status

---
+++ SelfGuidedTours.Core/Models/Dto/AllToursToAdminDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SelfGuidedTours.Core.Models.Dto
+{
+    public class AllToursToAdminDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Status { get; set; } = null!; 
+    }
+}
