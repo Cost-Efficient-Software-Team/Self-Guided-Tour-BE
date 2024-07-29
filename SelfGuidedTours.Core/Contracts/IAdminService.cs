26fa3e080e0189863c2eafeb3e24ac4b5edf645b
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-07-29 23:44:30 +0300

    Created AdminService, added it to IoC and implemented GetAllToursAsync's functionality.

---
+++ SelfGuidedTours.Core/Contracts/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using SelfGuidedTours.Core.Models;
+using SelfGuidedTours.Infrastructure.Data.Enums;
+
+namespace SelfGuidedTours.Core.Contracts
+{
+    public interface IAdminService
+    {
+        Task<ApiResponse> GetAllToursAsync(Status status);
+    }
+}
