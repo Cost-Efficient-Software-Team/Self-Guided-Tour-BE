d1d882f9347199ff303d24cb722b3ce77a3b2ccb
Михаел Йорданов <gaws@mail.bg>
2024-09-08 21:08:10 +0300

    Refactor logic to return and profile info

--- SelfGuidedTours.Core/Contracts/IUserProfile.cs
+++ SelfGuidedTours.Core/Contracts/IUserProfile.cs
@@ -1,13 +1,14 @@
 ﻿using SelfGuidedTours.Core.Models;
 using SelfGuidedTours.Core.Models.Dto;
+using SelfGuidedTours.Core.Models.RequestDto;
 using SelfGuidedTours.Infrastructure.Data.Models;
 
 namespace SelfGuidedTours.Core.Contracts
 {
     public interface IProfileService
     {
-        Task<UserProfileDto?> GetProfileAsync();
-        Task<UserProfile?> UpdateProfileAsync(Guid userId, UserProfile profile);
+        Task<UserProfileDto?> GetProfileAsync(string userId);
+        Task<UserProfileDto?> UpdateProfileAsync(string userId, UpdateProfileRequestDto profile);
         Task CreateProfileAsync(UserProfile userProfile);
     }
 }
---
+++ SelfGuidedTours.Core/Models/RequestDto/UpdateProfileRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Core.Models.RequestDto
+{
+    public class UpdateProfileRequestDto
+    {
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public IFormFile? ProfilePicture { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? AboutYourself { get; set; }
+        public string? Email { get; set; } = string.Empty;
+    }
+}
