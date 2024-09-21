3ab5817ea537befc568631dcbb60273e6884db79
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-09-21 18:45:25 +0300

    Added more informative description on every Entity's column and refactor some of the properties so it's more clear and easier to read and configure in future

--- SelfGuidedTours.Infrastructure/Migrations/20240921154216_AddedMoreInfoOnEachEntityColumn.Designer.cs
+++ SelfGuidedTours.Infrastructure/Migrations/20240921154216_AddedMoreInfoOnEachEntityColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SelfGuidedTours.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SelfGuidedTours.Infrastructure.Data;
 namespace SelfGuidedTours.Infrastructure.Migrations
 {
     [DbContext(typeof(SelfGuidedToursDbContext))]
-    partial class SelfGuidedToursDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240921154216_AddedMoreInfoOnEachEntityColumn")]
+    partial class AddedMoreInfoOnEachEntityColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
