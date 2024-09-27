fd8a91c45b614492576229866089174306f3f62a
Михаел Йорданов <gaws@mail.bg>
2024-09-27 07:05:23 +0300

    Create profile Response DTO

---
+++ SelfGuidedTours.Core/Models/ResponseDto/ProfileToursResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfGuidedTours.Core.Models.ResponseDto
+{
+    public class ProfileToursResponseDto
+    {
+        public int TourId { get; set; }
+        public string CreatorId { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Summary { get; set; } = null!;
+        public decimal? Price { get; set; }
+        public string Destination { get; set; } = null!;
+        public string ThumbnailImageUrl { get; set; } = null!;
+        public int EstimatedDuration { get; set; }
+        public string CreatorName { get; set; } = null!;
+        public string CreatedAt { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
