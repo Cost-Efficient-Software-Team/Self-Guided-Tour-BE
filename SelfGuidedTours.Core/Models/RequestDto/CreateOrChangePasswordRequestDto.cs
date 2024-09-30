a5cb29e014b8ecba19281adf5d765d4af9e3cf1f
Михаел Йорданов <gaws@mail.bg>
2024-10-01 00:14:54 +0300

    Refactor update logic

---
+++ SelfGuidedTours.Core/Models/RequestDto/CreateOrChangePasswordRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SelfGuidedTours.Common.ValidationConstants.AuthConstants.Register;
+namespace SelfGuidedTours.Core.Models.RequestDto
+{
+    public class CreateOrChangePasswordRequestDto
+    {
+        public string? CurrentPassword { get; set; }
+        [Required]
+        [RegularExpression(PasswordRegex,
+        ErrorMessage = InvalidPasswordMessage)]
+        public string NewPassword { get; set; } = null!;
+    }
+}
---
+++ SelfGuidedTours.Core/Models/RequestDto/CreatePasswordRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SelfGuidedTours.Common.ValidationConstants.AuthConstants.Register;
+namespace SelfGuidedTours.Core.Models.RequestDto
+{
+    public class CreatePasswordRequestDto
+    {
+        [Required]
+        [RegularExpression(PasswordRegex,
+        ErrorMessage = InvalidPasswordMessage)]
+        public string Password { get; set; } = null!;
+    }
+}
