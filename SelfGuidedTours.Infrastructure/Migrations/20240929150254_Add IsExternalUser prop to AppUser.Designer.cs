0d083835699faca5761455219bef32b93765346c
Михаел Йорданов <gaws@mail.bg>
2024-10-01 00:15:11 +0300

    Add isExternaUser prop to appuser

--- SelfGuidedTours.Infrastructure/Migrations/20240929150254_Add IsExternalUser prop to AppUser.Designer.cs
+++ SelfGuidedTours.Infrastructure/Migrations/20240929150254_Add IsExternalUser prop to AppUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SelfGuidedTours.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SelfGuidedTours.Infrastructure.Data;
 namespace SelfGuidedTours.Infrastructure.Migrations
 {
     [DbContext(typeof(SelfGuidedToursDbContext))]
-    partial class SelfGuidedToursDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240929150254_Add IsExternalUser prop to AppUser")]
+    partial class AddIsExternalUserproptoAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
